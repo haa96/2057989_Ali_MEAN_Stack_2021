8ce1b9e325efb2bb963e41bb4fbf97ebead6ae99
Ali Alsaif <54597354+haa96@users.noreply.github.com>
2021-08-17 09:17:01 -0500

    Add files via upload

---
+++ Phase2/portfolio/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule,ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
